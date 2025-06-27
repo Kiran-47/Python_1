c97d5c9f9c693e06a20ef189f1f2819a99771461
Kiran-47 <Kiranroka366@gmail.com>
2025-06-27 18:40:20 +0545

    git commit  #random ball

---
+++ Magic_8ball.py
@@ -0,0 +1,23 @@
+import random
+num = random.randint(0,9)
+print("BALL NUMBER:", num)
+if num == 0:
+    print("It is GOOD DAY  FOR YOU ")
+elif num == 1:
+    print("It is NOT GOOD DAY  FOR YOU ")
+elif num == 2:
+    print("It is NOT GOOD DAY  FOR YOU ")   
+elif num == 3:
+    print("It is NOT GOOD DAY  FOR YOU ")
+elif num == 4:
+    print("It is NOT GOOD DAY  FOR YOU ")
+elif num == 5:
+    print("It is NOT GOOD DAY  FOR YOU ")
+elif num == 6:
+    print("It is NOT GOOD DAY  FOR YOU ")
+elif num == 7:
+    print("It is NOT GOOD DAY  FOR YOU ")   
+elif num == 8:
+    print("It is NOT GOOD DAY  FOR YOU ")
+elif num == 9: 
+    print("It is NOT GOOD DAY  FOR YOU ")
